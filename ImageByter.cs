392fc74fc6c916165c5271867700119bc7c1d9f2
lomstfer <90508408+lomstfer@users.noreply.github.com>
2023-10-05 17:26:23 +0200

    works

---
+++ ImageByter.cs
@@ -0,0 +1,68 @@
+using System.Runtime.InteropServices;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Collections;
+
+[System.Runtime.Versioning.SupportedOSPlatform("windows")]
+public class ImageByter {
+    private byte[] _bytes = new byte[0];
+
+    public byte[] Bytes {
+        get {
+            return _bytes;
+        }
+        set {
+            _bytes = value;
+            _bits = new BitArray(value);
+        }
+    }
+
+    private BitArray _bits;
+
+    public BitArray Bits {
+        get {
+            return _bits;
+        }
+        set {
+            _bits = value;
+            _bytes = BitArrayToByteArray(value);
+        }
+    }
+
+    private Bitmap inputImage;
+    private IntPtr pointeur;
+    private BitmapData bmpData;
+
+    public ImageByter(string fileName) {
+        FileStream bmpstream = new FileStream(fileName, FileMode.Open, FileAccess.Read);
+        inputImage = new Bitmap(bmpstream);
+
+        Rectangle rect = new Rectangle(0, 0, inputImage.Width, inputImage.Height);
+        bmpData = inputImage.LockBits(rect, ImageLockMode.ReadWrite, inputImage.PixelFormat);
+        pointeur = bmpData.Scan0;
+        int bytesLength = Math.Abs(bmpData.Stride) * bmpData.Height;
+        
+        _bytes = new byte[bytesLength];
+
+        Marshal.Copy(pointeur, _bytes, 0, bytesLength);
+
+        _bits = new BitArray(_bytes);
+    }
+
+    public void SaveFile(string outputFileName) {
+        if (inputImage == null || bmpData == null)
+            return;
+
+        Marshal.Copy(_bytes, 0, pointeur, _bytes.Length);
+
+        inputImage.UnlockBits(bmpData);
+
+        inputImage.Save(outputFileName);
+    }
+
+    public static byte[] BitArrayToByteArray(BitArray bits) {
+        byte[] bytes = new byte[(bits.Count - 1) / 8 + 1];
+        bits.CopyTo(bytes, 0);
+        return bytes;
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Runtime.InteropServices;
+
+if (!RuntimeInformation.IsOSPlatform(OSPlatform.Windows)) {
+    Console.WriteLine("only windows is supported lmao");
+    return;
+}
+Random random = new();
+
+if (args == null || args.Length < 2) {
+    Console.WriteLine("provide input file and output file. example: cim in.jpg out.jpg");
+    return;
+}
+
+string filename = args[0];
+if (!File.Exists(filename)) {
+    Console.WriteLine("input file not found");
+    return;
+}
+
+ImageByter imgb = new(filename);
+
+BitArray bits = imgb.Bits;
+
+int levels = 7;
+if (args != null && args.Length >= 3) 
+    int.TryParse(args[2], out levels);
+
+for (int level = 0; level < levels; level++) {
+    for (int i = 0; i < imgb.Bits.Length; i += 8) {
+        bool r = random.Next(2) == 1;
+        bits.Set(i + level, r);
+    }
+}
+
+
+imgb.Bits = bits;
+
+imgb.SaveFile(args[1]);
